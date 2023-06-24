3e1a2735a7dc9016821ecc1d02e9e072304f1233
sozankhalil <sozan.civil@gmail.com>
2023-06-24 18:20:37 +0300

    first commit

---
+++ greeting.js
@@ -0,0 +1,12 @@
+export const informalGreeting = (person)=>{
+    console.log(`hello ${person}`)
+}
+export const formalGreeting =(person)=>{
+    console.log(`good day ${person}`)
+ 
+}
+
+const greeting =()=>{
+console.log('howdyyy')
+}
+export default greeting;
---
+++ webpageContentUpdate.js
@@ -0,0 +1,47 @@
+// let answer = prompt('What is your name?');
+// if (typeof(answer) === 'string') {
+//     var h1 = document.createElement('h1')
+//     h1.innerText = answer;
+//     document.body.innerText = '';
+//     document.body.appendChild(h1);
+// }
+
+
+// var h1 = document.createElement('h1')
+// h1.innerText = "Type into the input to make this text change"
+
+// var input = document.createElement('input')
+// input.setAttribute('type', 'text')
+
+// document.body.innerText = '';
+// document.body.appendChild(h1);
+// document.body.appendChild(input);
+
+// input.addEventListener('change', function() {
+//     h1.innerText=input.value;
+// })
+
+var h1 = document.querySelector('h1')
+var arr = [
+        'Example Domain',
+        'First Click',
+        'Second Click',
+        'Third Click'
+    ]
+
+   function handleClicks() {
+    switch(h1.innerText) {
+        case arr[0]:
+            h1.innerText = arr[1]
+            break
+        case arr[1]:
+            h1.innerText = arr[2]
+            break
+        case arr[2]:
+            h1.innerText = arr[3]
+            break
+        default:
+     h1.innerText = arr[0]
+ }
+}
+h1.addEventListener('click', handleClicks);
